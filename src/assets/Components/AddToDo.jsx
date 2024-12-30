cf4b75c0b3a2cc3e3daf48dd395fe41c51b5fc78
Samridhi Tripathi <stripath32@gmail.com>
2024-12-30 11:13:02 +0000

    Deploy updated build with correct script paths

--- src/assets/Components/AddToDo.jsx
+++ src/assets/Components/AddToDo.jsx
@@ -41,7 +41,7 @@ export default function AddToDo({handleClicks}){
           </div>
           <div className="col-2">
           <button type="button" className="btn btn-success" onClick={handleAddBtn}><MdAlarmAdd /> Add 
-         < <ToastContainer  closeButton={false} theme="dark"/>
+          <ToastContainer  closeButton={false} theme="dark"/>
           </button>
           </div>
           </div>
