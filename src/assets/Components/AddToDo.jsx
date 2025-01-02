7b03c01c8548fafc53e242c7b300daedca46e86b
Samridhi Tripathi <stripath32@gmail.com>
2025-01-02 23:22:45 +0530

    Update AddToDo.jsx

--- src/assets/Components/AddToDo.jsx
+++ src/assets/Components/AddToDo.jsx
@@ -1,49 +1,74 @@
-import { useState } from "react"
+// import { useState } from "react"
+import { useRef } from "react";
 import styles from './AddToDo.module.css'
 import { MdAlarmAdd } from "react-icons/md";
 import { ToastContainer, toast } from 'react-toastify';
 export default function AddToDo({handleClicks}){
 
-  const [todoName,setToDoname]=useState('')
-  const[dueDate,setDueDate]=useState('')
+  // const [todoName,setToDoname]=useState('')
+  // const[dueDate,setDueDate]=useState('')
 
-  const handleNameChange=(e)=>{
-    setToDoname(e.target.value)
-  }
-  const handleDateChange=(e)=>{
-    setDueDate(e.target.value)
+  // const handleNameChange=(e)=>{
+  //   setToDoname(e.target.value)
+  // }
+  // const handleDateChange=(e)=>{
+  //   setDueDate(e.target.value)
 
-  }
+  // }
 
-
+ const currName=useRef()
+  const currDate=useRef()
+  
   const handleAddBtn=(event)=>{
-    if(todoName==''){
+   //  if(todoName==''){
+   //    toast.error("Enter some task!");
+   //    return
+   //  }
+   // if(dueDate==''){
+   //  toast.error("Enter date!");
+   //  return
+   // }
+    // handleClicks(todoName,dueDate)
+    // setDueDate('')
+    // setToDoname('')
+    // toast.success("Added!");
+      const handleAddBtn=(event)=>{
+   event.preventDefault()
+    if(currName.current.value==''){
       toast.error("Enter some task!");
       return
     }
-   if(dueDate==''){
+   if(currDate.current.value==''){
     toast.error("Enter date!");
     return
    }
-   event.preventDefault()
-    handleClicks(todoName,dueDate)
-    setDueDate('')
-    setToDoname('')
+    const toDoName= currName.current.value
+    const dueDate= currDate.current.value
+    handleClicks(toDoName,dueDate)
+    currDate.current.value=''
+    currName.current.value=''
+    console.log(toast);
     toast.success("Added!");
   }
 
      return <>
+      <ToastContainer theme="dark"  />
       <form className="container text-center row flex align-items-center" onSubmit={handleAddBtn}>
           <div className="col-6">
-            <input className={styles.inputText} type="text" placeholder="Enter some task" onChange={handleNameChange} value={todoName}/>
+            <input className={styles.inputText} type="text" placeholder="Enter some task"
+               ref={currName}
+{/*               onChange={handleNameChange} value={todoName} */}
+              />
           </div>
           <div className="col-4">
-            <input className={styles.date} type="date" placeholder="Select date" onChange={handleDateChange} value={dueDate}/>
+            <input className={styles.date} type="date" placeholder="Select date" 
+              ref={currDate}
+{/*               onChange={handleDateChange} value={dueDate} */}
+              />
           </div>
           <div className="col-2">
           <button className="btn btn-success" ><MdAlarmAdd /> Add 
           </button>
-          <ToastContainer theme="dark" />
           </div>
           </form>
      </>
